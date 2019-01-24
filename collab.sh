a567b40581af060ce5df7dae19568c65c6b5632c
anjalysuresh <anjalysuresh@yahoo.co.in>
2019-01-24 17:15:25 +0530

    Update collab.sh

--- collab.sh
+++ collab.sh
@@ -3,4 +3,4 @@ sudo docker-compose down
 sudo docker-compose up -d db
 sudo docker-compose build        
 sudo docker-compose up -d db
-sudo docker exec -i pipeline_multirepo_db_1 mysql -uroot -proot django < collab-updated.sql
+sudo docker exec -i places_of_worship_db_1 mysql -uroot -proot django < collab-updated.sql
